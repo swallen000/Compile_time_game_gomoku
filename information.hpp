898db84b995c5d93157a69d4e86063a9d6d6171b
ShaoChiWu <wsc861029@gmail.com>
2021-04-12 21:15:57 -0400

    first commit

--- information.hpp
+++ information.hpp
@@ -4,7 +4,7 @@
 
 constexpr int judge(int &x, int &y, int &p, bool &placed);
 
-// read current.txt to array
+// read current.txt to array (std::string_view)
 constexpr void set_array_sv(){
     for(int i=1, current = 0; i<row-2; ++i){
         auto sub = game_string.substr(i*(column+1)+1, column);
@@ -14,68 +14,79 @@ constexpr void set_array_sv(){
     }
 }
 
+// read current.txt to array (STR)
 constexpr void set_array(){
-    int current = 0;
-    auto sub1 = test_string.substr<1*(column+1)+1, column>();
-    for(int j=0; j<column; ++j)
-            chessboard[current][j] = sub1[j];
-    current++;
-    auto sub2 = test_string.substr<2*(column+1)+1, column>();
-    for(int j=0; j<column; ++j)
-            chessboard[current][j] = sub2[j];
-    current++;
-    auto sub3 = test_string.substr<3*(column+1)+1, column>();
-    for(int j=0; j<column; ++j)
-            chessboard[current][j] = sub3[j];
-    current++;
-    auto sub4 = test_string.substr<4*(column+1)+1, column>();
-    for(int j=0; j<column; ++j)
-            chessboard[current][j] = sub4[j];
-    current++;
-    auto sub5 = test_string.substr<5*(column+1)+1, column>();
-    for(int j=0; j<column; ++j)
-            chessboard[current][j] = sub5[j];
-    current++;
-    auto sub6 = test_string.substr<6*(column+1)+1, column>();
-    for(int j=0; j<column; ++j)
-            chessboard[current][j] = sub6[j];
-    current++;
-    auto sub7 = test_string.substr<7*(column+1)+1, column>();
-    for(int j=0; j<column; ++j)
-            chessboard[current][j] = sub7[j];
-    current++;
-    auto sub8 = test_string.substr<8*(column+1)+1, column>();
-    for(int j=0; j<column; ++j)
-            chessboard[current][j] = sub8[j];
-    current++;
-    auto sub9 = test_string.substr<9*(column+1)+1, column>();
-    for(int j=0; j<column; ++j)
-            chessboard[current][j] = sub9[j];
+    for(int i=1, current = 0; i<row-2; ++i){
+        auto sub = test_string.substr<i*(column+1)+1, column>();
+        for(int j=0; j<column; ++j)
+            chessboard[current][j] = sub[j];
+        current++;
+    }
 }
 
-// move the current position according to the keyboard input
-template<class T>
-constexpr bool move_chessboard(T& input, int& x, int& y){
-    if(chessboard[y-1][x*2] == '+')
-        chessboard[y-1][x*2] = ' ';
-    if(input == User_Input::Top && y > 1){
-        y--;
-    }
-    else if(input == User_Input::Down && y < array_size){
-        y++;
-    }
-    else if(input == User_Input::Left && x > 1){
-        x--;
-    }
-    else if(input == User_Input::Right && x < array_size){
-        x++;
-    }
-    else if(input == User_Input::Space){
-        if(chessboard[y-1][x*2] == 'O' || chessboard[y-1][x*2] == '@')
-            return false;
-    }
-    return true;
-}
+// // read current.txt to array (STR)
+// constexpr void set_array(){
+//     int current = 0;
+//     auto sub1 = test_string.substr<1*(column+1)+1, column>();
+//     for(int j=0; j<column; ++j)
+//             chessboard[current][j] = sub1[j];
+//     current++;
+//     auto sub2 = test_string.substr<2*(column+1)+1, column>();
+//     for(int j=0; j<column; ++j)
+//             chessboard[current][j] = sub2[j];
+//     current++;
+//     auto sub3 = test_string.substr<3*(column+1)+1, column>();
+//     for(int j=0; j<column; ++j)
+//             chessboard[current][j] = sub3[j];
+//     current++;
+//     auto sub4 = test_string.substr<4*(column+1)+1, column>();
+//     for(int j=0; j<column; ++j)
+//             chessboard[current][j] = sub4[j];
+//     current++;
+//     auto sub5 = test_string.substr<5*(column+1)+1, column>();
+//     for(int j=0; j<column; ++j)
+//             chessboard[current][j] = sub5[j];
+//     current++;
+//     auto sub6 = test_string.substr<6*(column+1)+1, column>();
+//     for(int j=0; j<column; ++j)
+//             chessboard[current][j] = sub6[j];
+//     current++;
+//     auto sub7 = test_string.substr<7*(column+1)+1, column>();
+//     for(int j=0; j<column; ++j)
+//             chessboard[current][j] = sub7[j];
+//     current++;
+//     auto sub8 = test_string.substr<8*(column+1)+1, column>();
+//     for(int j=0; j<column; ++j)
+//             chessboard[current][j] = sub8[j];
+//     current++;
+//     auto sub9 = test_string.substr<9*(column+1)+1, column>();
+//     for(int j=0; j<column; ++j)
+//             chessboard[current][j] = sub9[j];
+// }
+
+// // move the current position according to the keyboard input
+// template<class T>
+// constexpr bool move_chessboard(T& input, int& x, int& y){
+//     if(chessboard[y-1][x*2] == '+')
+//         chessboard[y-1][x*2] = ' ';
+//     if(input == User_Input::Top && y > 1){
+//         y--;
+//     }
+//     else if(input == User_Input::Down && y < array_size){
+//         y++;
+//     }
+//     else if(input == User_Input::Left && x > 1){
+//         x--;
+//     }
+//     else if(input == User_Input::Right && x < array_size){
+//         x++;
+//     }
+//     else if(input == User_Input::Space){
+//         if(chessboard[y-1][x*2] == 'O' || chessboard[y-1][x*2] == '@')
+//             return false;
+//     }
+//     return true;
+// }
 
 
 constexpr int read_input(){
@@ -139,24 +150,22 @@ constexpr int read_input(){
                 std::cout<<"player "<<p<<"'s turn\n";
         }
     }
+
     int result = judge(x, y, p, placed);
+
     if(result==12){
         std::cout<<"DRAW!\n";
-        //std::cout<<")\"";
         return 12;
     }
     else if(result==1){
         std::cout<<"O Win!\n";
-        //std::cout<<")\"";
         return 1;
     } 
     else if(result==2){
         std::cout<<"@ Win!\n";
-        //std::cout<<")\"";
         return 2;
     }
     else std::cout<<"Game moving on...\n";
-    //std::cout<<")\"";
     return 0;
 }
 
@@ -247,82 +256,82 @@ constexpr int read_input_sv(){
 // Judge
 constexpr int judge(int &x, int &y, int &p, bool &placed){
     if(placed){
+        int count = 0;
+        int r = y-1; // 0~8
+        int c = x*2; // 2~18
+        char sign[3] = {' ', 'O','@'};
 
-    int count = 0;
-    int r = y-1; // 0~8
-    int c = x*2; // 2~18
-    char sign[3] = {' ', 'O','@'};
-    // Check Vertical WIN
-    while(r>=0 && chessboard[r][c]==sign[p]){
-        count++;
-        r--;
-        if(count==5) return p;
-    }
-    r = y-1+1;
-    while(r<array_size && chessboard[r][c]==sign[p]){
-        count++;
-        r++;
-        if(count==5) return p;
-    }
+        // Check Vertical WIN
+        while(r>=0 && chessboard[r][c]==sign[p]){
+            count++;
+            r--;
+            if(count==5) return p;
+        }
+        r = y-1+1;
+        while(r<array_size && chessboard[r][c]==sign[p]){
+            count++;
+            r++;
+            if(count==5) return p;
+        }
 
-    r = y-1;
-    count = 0;
-    // Check Horizontal WIN
-    while(c>=2 && chessboard[r][c]==sign[p]){
-        count++;
-        c -= 2;
-        if(count==5) return p;
-    }
-    c = x*2+2;
-    while(c<=array_size*2 && chessboard[r][c]==sign[p]){
-        count++;
-        c += 2;
-        if(count==5) return p;
-    }
+        r = y-1;
+        count = 0;
+        // Check Horizontal WIN
+        while(c>=2 && chessboard[r][c]==sign[p]){
+            count++;
+            c -= 2;
+            if(count==5) return p;
+        }
+        c = x*2+2;
+        while(c<=array_size*2 && chessboard[r][c]==sign[p]){
+            count++;
+            c += 2;
+            if(count==5) return p;
+        }
     
-    c = x*2;
-    count = 0;
-    //Check Left Cross WIN
-    while(r>=0 && c>=2 && chessboard[r][c]==sign[p]){
-        count++;
-        r--;
-        c -= 2;
-        if(count==5) return p;
-    }
-    r = y-1+1;
-    c = x*2+2;
-    while(r<array_size && c<=array_size*2 && chessboard[r][c]==sign[p]){
-        count++;
-        r++;
-        c += 2;
-        if(count==5) return p;
-    }
+        c = x*2;
+        count = 0;
+        //Check Left Cross WIN
+        while(r>=0 && c>=2 && chessboard[r][c]==sign[p]){
+            count++;
+            r--;
+            c -= 2;
+            if(count==5) return p;
+        }
+        r = y-1+1;
+        c = x*2+2;
+        while(r<array_size && c<=array_size*2 && chessboard[r][c]==sign[p]){
+            count++;
+            r++;
+            c += 2;
+            if(count==5) return p;
+        }
 
-    r = y-1;
-    c = x*2;
-    count = 0;
-    //Check Right Cross WIN
-    while(r>=0 && c<=array_size*2 && chessboard[r][c]==sign[p]){
-        count++;
-        r--;
-        c += 2;
-        if(count==5) return p;
-    }
-    r = y-1+1;
-    c = x*2-2;
-    while(r<array_size && c>=2 && chessboard[r][c]==sign[p]){
-        count++;
-        r++;
-        c -= 2;
-        if(count==5) return p;
-    }
+        r = y-1;
+        c = x*2;
+        count = 0;
+        //Check Right Cross WIN
+        while(r>=0 && c<=array_size*2 && chessboard[r][c]==sign[p]){
+            count++;
+            r--;
+            c += 2;
+            if(count==5) return p;
+        }
+        r = y-1+1;
+        c = x*2-2;
+        while(r<array_size && c>=2 && chessboard[r][c]==sign[p]){
+            count++;
+            r++;
+            c -= 2;
+            if(count==5) return p;
+        }
 
-    // Check DRAW
-    int num=0;
-    for(int i=0; i<array_size; ++i)
-        for(int j=1; j<=array_size; ++j)
-            if(chessboard[i][2*j]=='O'||chessboard[i][2*j]=='@') num++;
-    if(num==array_size*array_size) return 12;
+        // Check DRAW
+        int num=0;
+        for(int i=0; i<array_size; ++i)
+            for(int j=1; j<=array_size; ++j)
+                if(chessboard[i][2*j]=='O'||chessboard[i][2*j]=='@') num++;
+        if(num==array_size*array_size) return 12;
 
     }
     return 0;
--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 #include "information.hpp"
 
-
 using namespace std;
 
 int main(){
     static_assert(game_string.size() > 0, "game string is null!");
     static_assert(array_size > 0, "incorrect array size");
-    set_array();
+    set_array(); //using STR
+    return read_input(); //using STR
+    //set_array_sv(); // using string_view
+    //return read_input_sv(); // using string_view
     
-    //set_array_sv();
-
-    //return read_input_sv(); 
-    return read_input();  
 }
